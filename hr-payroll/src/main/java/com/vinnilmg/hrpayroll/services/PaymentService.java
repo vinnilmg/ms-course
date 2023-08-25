da7b69498cbda8998400b1057af850ba0b9b8058
vinnilmg <vini.delimagomes@gmail.com>
2023-08-24 21:38:58 -0300

    feign client

--- hr-payroll/src/main/java/com/vinnilmg/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/vinnilmg/hrpayroll/services/PaymentService.java
@@ -2,25 +2,18 @@ package com.vinnilmg.hrpayroll.services;
 
 import com.vinnilmg.hrpayroll.entities.Payment;
 import com.vinnilmg.hrpayroll.entities.Worker;
+import com.vinnilmg.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = Map.of("id", String.valueOf(workerId));
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
